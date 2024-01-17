846cfc04cc66b4b3c4662836695b667b17fad5a0
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-01-17 23:17:00 +0530

    Add files via upload

---
+++ Condition5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Condition5{
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Value of button is");
+        int button = sc.nextInt();
+        if (button == 1){
+            System.out.println("hello");
+        }   else if (button == 2){
+                    System.out.println("Hi");
+
+            }
+            else if (button == 3){
+                System.out.println("Sehaju");
+            }
+        
+                else{
+                    System.out.println("B is greater");
+                }
+            
+        
+ }   }
+}
