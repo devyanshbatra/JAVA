818a66592de8cb109c98819edee338e301b475de
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-01-17 23:09:01 +0530

    Add files via upload

---
+++ Condition2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Condition2{
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x % 2 ==0){
+            System.out.println("nUMBER IS EVEN");
+
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
---
+++ Condition3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Condition3{
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Value of a is");
+       
+        int a = sc.nextInt();
+        System.out.println("Value of b is");
+        int b = sc.nextInt();
+        if (a == b){
+            System.out.println("Equal");
+            } else {
+                if (a > b){
+                    System.out.println("A is greater than B");
+
+                }
+                else{
+                    System.out.println("B is greater");
+                }
+            
+        }
+    }
+}
