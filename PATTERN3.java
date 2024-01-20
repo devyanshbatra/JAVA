a84ab73b8b181594e0328ce0063a144aae6e9072
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-01-20 18:40:53 +0530

    Add files via upload

--- PATTERN3.java
+++ PATTERN3.java
@@ -5,7 +5,7 @@ public class Patterns2 {
       System.out.println("Enter the number of stars lines u want");
        int n = sc.nextInt();
       
-       for(int i=1; i<=n; i++) {
+       for(int i=n; i>=1; i--) {
            for(int j=1; j<=i; j++) {
                    System.out.print("*");
            }
