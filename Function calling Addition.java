d1e14bf1b37a2e6e8e6b6189f61cc7852680df0c
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-01-25 22:01:28 +0530

    Add files via upload

--- Function calling Addition.java
+++ Function calling Addition.java
@@ -1,14 +1,20 @@
 public class Main {
     //A METHOD to calculate sum of 2 numbers - a & b
-    public static void sum(int a, int b) {
+    public static int calculatedsum(int a, int b) {
         int sum = a + b;
         System.out.println(sum);
     }
    
     public static void main(String[] args) {
-       int a = 10;
-       int b = 20;
-        sum(a, b); // Function Call
+       Scanner sc = new Scanner(System.in);
+       int a =sc.nextInt();
+       int b = sc.nextInt();
+       int sum = calculatedsum(a,b);
+       system.out.println("The sum of the nubers entered is "+sum);
+       
+
+
+        
  
     }
  
