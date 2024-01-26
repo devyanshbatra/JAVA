cb1d5e5697be818eafa04de273784a5d9c7333db
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-01-26 10:54:06 +0530

    Add files via upload

---
+++ Function Multiplication.java
@@ -0,0 +1,13 @@
+public class function{
+    public static int calculatedproduct(int a, int b) {
+       int product = a*b;
+       return product;
+    }
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+       int a =sc.nextInt();
+       int b = sc.nextInt();
+       int product = calculated product(a,b);
+       System.out.println("The productof the two numbers is"+product);
+    }
+}
