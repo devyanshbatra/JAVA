f17ffd1ff76ed49dbc7ae70366c1818c9309f062
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-01-28 22:13:56 +0530

    Add files via upload

---
+++ ARRAYS1.java
@@ -0,0 +1,12 @@
+public class Arrays{
+    public static void main(String[] args ) 
+    {
+    int[] marks = new int[2];
+    marks[0]=78;
+    marks[1]=88;
+       for(int i=0,i<2,i++)
+       {
+       System.out.println("marks[i]");
+       }
+    }
+}
