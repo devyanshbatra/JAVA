1bf6a9f434fc3b9fcc08fe8d1f00b915ac52a964
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-02-18 20:10:50 +0530

    Add files via upload

---
+++ Accessing Particular Element.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Strings{
+    public static void main (String args[] ) {
+        Scanner.sc = new Scanner(System.in);
+        System.out.println=("Enter the first string");
+        String first = sc.next();
+        system.out.println("Enter the last string");
+        String last = sc.next();
+        String new = first + last;
+        system.out.println("new.length()")
+        
+
+      for(int i=0; i<new.length(); i++) 
+      {
+        System.out.println(fullName.charAt(i));
+      }
+
+
+    }
+}
---
+++ Comparison Of strings.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Strings{
+    public static void main (String args[] ) {
+        Scanner.sc = new Scanner(System.in);
+        System.out.println=("Enter the first string");
+        String first = sc.next();
+        system.out.println("Enter the last string");
+        String last = sc.next();
+    if(first == last){
+        system.out.println("The strings enetered are equal");
+
+       } else{
+        system.out.println("The strings are not equal");
+       }
+    }   }
