12b82e5e8701d16c27e0e5555140d44af083a1a7
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-02-18 16:45:10 +0530

    Add files via upload

---
+++ 2D ARRAYS part 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class TwoDArrays {
+    public static void main(Strings args[]) {
+        scanner.sc = new scanner(System.in);
+        System.out.println("Enter the number of rows")
+        int rows = sc.nextInt();
+        system.out.println("Enter the number of columns");
+        int column = sc.nextInt();
+        int [][] numbers = new int[rows][cols];
+        for(int i=0;i=rows;i++) {
+            for (int j=0;j=cols;j++) {
+              numbers[i][j] = sc.nextInt
+            }
+        }
+        for(int i=0;i=rows;i++) {
+            for (int j=0;j=cols;j++){
+                System.out.print(numbers[i][j]+" ");
+                
+            }
+            system.out.println();
+        }     
+
+
+    }
+}
