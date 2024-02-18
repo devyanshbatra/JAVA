2b1ebd32d2b3e625eb2f1a20ecdc477c512582a2
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-02-18 17:09:31 +0530

    Add files via upload

---
+++ STRINGS.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Strings{
+    public static void main (String args[] ) {
+        Scanner.sc = new Scanner(System.in);
+        string name = sc.next();
+        System.out.println("The string enetered is:"+name);
+
+    }
+}
