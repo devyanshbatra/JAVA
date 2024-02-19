38a071f7243c2a265825f43b77e4194a7d1c41a0
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-02-19 22:53:22 +0530

    Add files via upload

---
+++ Sub_strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sub_Strings{
+    public static void main (String args[] ) {
+        Scanner.sc = new Scanner(System.in);
+        System.out.println("Enter the string sentence");
+        String sentence = "My name is Devyansh" 
+        // the substring function takes the 
+        //begining index and the final index which are inclusive
+        String name = sentence.substring(11,sentence.length());
+        System.out.println(name)
+}   }
