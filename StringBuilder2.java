30a9c09eb670b4608fff826d0209a07d78eebbc3
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-02-19 23:08:56 +0530

    Add files via upload

---
+++ StringBuilder2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringBuilder{
+    public static void main (String args[] ) {
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb.charAt(2));
+        sb.setCharAt(0,"p");
+    
+
+    }
+}
