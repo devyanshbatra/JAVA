ce6faae7dc16d5c386d57adc27dbf20304692d7f
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-02-20 23:15:43 +0530

    Add files via upload

---
+++ StringBuilder3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Strings3{
+    public static void main (String args[] ) {
+    Scanner sc = new.Scanner(system.in)
+    StringBuilder sb = new StringBuilder("Tony");
+    system.out.println(sb);
+    sb.insert(2,"m");
+    system.out.println(sb);
+    sb.delete(2,3);
+    system.out.println(sb);
+    }
+}'
