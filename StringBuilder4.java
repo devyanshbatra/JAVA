da745eb675c5774fb18d138b65fbaff71744a901
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-02-25 10:13:40 +0530

    Add files via upload

---
+++ StringBuilder4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Strings3{
+    public static void main (String args[] ) {
+    Scanner sc = new.Scanner(system.in)
+    StringBuilder sb = new StringBuilder("h");
+    sb.append("e");
+    sb.append("l");
+    sb.append("l");
+    system.out.println("sb.length()");
