ffb6f0d3b692c5415f5f62a905c020a520777dcd
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-05 14:07:21 +0530

    Add files via upload

---
+++ Grades.java
@@ -0,0 +1,21 @@
+import java.util.*;
+Public class grading
+public static void main (String args[] ) {
+    Scanner.sc = new Scanner(System.in);
+    System.out.println("Enter the marks in physics");
+    int P = sc.nextInt();
+    System.out.println("Enter the marks in chemistry");
+    int C = sc.nextInt();
+    System.out.println("Enter the marks in maths");
+    int M = sc.nextInt();
+    if(P<50||C<50||M<50)
+    {
+        System.out.println("The student has failed in the eexams");
+
+    }
+    else if {
+        int total_marks = P + C + M 
+        float percentage = total_marks / 300 * 100
+        
+
+    }
