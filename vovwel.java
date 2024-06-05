5b311712b84c9a1d596b3856ce4dbdaf39857762
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-05 14:31:05 +0530

    Add files via upload

---
+++ vovwel.java
@@ -0,0 +1,26 @@
+import java.util.*;
+Public class vovwel
+public static void main (String args[] ) {
+    Scanner.sc = new Scanner(System.in);
+    String word = sc.next();
+    System.out.println("enter the word to be checked whether if vowel or consonant");
+    switch(word) {
+        case a:
+        System.out.println("The entered element is vowel");
+        break;
+        case e:
+        System.out.println("The entered element is vowel");
+        break;
+        case i:
+        System.out.println("The entered element is vowel");
+        break;
+        case o:
+        System.out.println("The entered element is vowel");
+        break;
+        case u:
+        System.out.println("The entered element is vowel");
+        break;
+        default
+        System.out.println("THE WORD ENETERED IS CONSONANT");
+    }
+}
