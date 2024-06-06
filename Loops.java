cdc2bdbe6521569fe0a59b59cf62f446693595d9
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-06 17:03:37 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class loops{
+ public static void main (String args[] ) {
+    Scanner.sc = new Scanner(System.in);
+    int n ;
+    int sum=0;
+    int i;
+
+    for(i=0;i<=9;i++)
+    {
+    System.out.println("Enter the number");
+    int n =sc.nextInt();
+    sum = sum+n
+    }
+   System.out.println("THE SUM OF THE 10 NUMBER IS"+sum);
+   
+
+}
+}
+    
