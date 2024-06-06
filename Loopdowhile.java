d6418cea3a6fd9b650c84d6b99a315a6d7618168
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-06 22:38:22 +0530

    Add files via upload

---
+++ Loopdowhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Loopdowhile {
+    public static void main(String[] args ){
+         Scanner sc = new Scanner(System.in);
+         int i = sc.nextInt();
+         int j;
+         for (j=0;j<=10;j++)
+         {
+            int product = i*j
+            System.out.println("The table of the number is"+product);
+
+         }
+
+    }
+
+}
