fb9d25a6f73e3cc41abd2d1530dda2f213d2cf17
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-06 17:07:54 +0530

    Add files via upload

---
+++ LoopWhile.java
@@ -0,0 +1,10 @@
+public class LoopWhile{
+    public static void main(String[] args ){
+     int i = 0;
+     int j;
+        while(j<=10){
+          i=i+1;
+          System.out.println("The count of the number is "+i);
+        }
+    }
+}
