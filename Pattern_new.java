cd1a39c76eda393f83dd5f00009b7d796bc86fe9
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-07 19:30:03 +0530

    Add files via upload

---
+++ Pattern_new.java
@@ -0,0 +1,16 @@
+public class pattern {
+    public static void main(Strings args[])
+    scanner.sc = new scanner(System.in);
+    int row = sc.nextInt();
+    int columns = sc.nextInt();
+    for (row=1;row<=4;row++)
+    {
+        System.out.println();
+    }
+    for(columns=1;columns<=4;columns++)
+    {
+        System.out.println(j);
+        
+    }
+
+}
