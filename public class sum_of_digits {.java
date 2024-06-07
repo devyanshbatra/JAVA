609bb52859f1e9fbb1c2e0ea2bd9439958e65c90
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-07 13:48:28 +0530

    Add files via upload

---
+++ public class sum_of_digits {.java
@@ -0,0 +1,18 @@
+public class sum_of_digits {
+    public static void main(Strings args[]) {
+        scanner.sc = new scanner(System.in);
+        System.out.println("Enter the number")
+        int num = sc.nextInt;
+        int r;
+        int sum;
+        int s =0;
+        while(num!=0)
+        {
+         r = r%10;
+         sum=s+r;
+         n=n%10
+        }
+    System.out.print("The sum of the digits is "+sum)
+    }
+    
+}
