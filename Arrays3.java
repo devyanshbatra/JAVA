f1f6feb30efbf2c803afdb9cb0a2b43891094a10
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-15 00:11:25 +0530

    Add files via upload

---
+++ Arrays3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Arrays2{
+   public static void main(String args[]){
+   Scanner sc = new Scanner (system.in)
+   int n[]=new int[10]
+   for(i=0;i<=n;i++)
+   {
+      System.out.print("Enter thee number to put in the array");
+      n[i]=sc.nextInt();
+      
+
+   }
+   
+
+   }
+}
---
+++ Repeating_in_arrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Arrays2{
+   public static void main(String args[]){
+   Scanner sc = new Scanner (system.in)
+   int n[]=new int[10]
+   int l;
+   for(i=0;i<=n;i++)
+   { 
+      System.out.print("Enter thee number to put in the array");
+      n[i]=sc.nextInt();
+   }
+     n[0]= int l
+     if(n[i==l])
+     {
+      l=n[i]
+      system.oout.println("The number is repeated")
+     }
+   }
+}
+   
