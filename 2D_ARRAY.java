291d9e8838db539ef41562a66bedb025a2dad156
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-17 17:18:29 +0530

    Add files via upload

---
+++ 2D_ARRAY.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Arrays2{
+   public static void main(String args[]){
+   Scanner sc = new Scanner (system.in);
+   int n[i][j]=new int[5][5]
+   for(i=0;i<=5;i++)
+   {
+    for(j=0;j<=5;j++)
+    {
+        System.out.println("Enter the number in the array");
+        n[i][j]=sc.nextInt();
+    }
+   }
+   for(i=0;i<=5;i++)
+   {
+    for(j=0;j<=5;j++)
+    {
+        System.out.println("The 2d array is "+n[i][j])
+    }
+   }
+   }
+}
