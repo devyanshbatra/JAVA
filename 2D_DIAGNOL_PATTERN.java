16523d854c2f2882c4c725d4a27a407a00151f6e
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-17 17:23:22 +0530

    Add files via upload

---
+++ 2D_DIAGNOL_PATTERN.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class 2DArrays{
+   public static void main(String args[]){
+   Scanner sc = new Scanner (system.in);
+   int n[i][j]=new int[5][5]
+   for(i=0;i<=5;i++)
+   {
+    for(j=0;j<=5;j++)
+    {
+        System.out.println("Enter the number in the array");
+        n[i][j]=sc.nextInt();
+    }
+   }
+     for(i=0;i<=5;i++)
+     {
+        for(j=0;j<=5;j++)
+        {
+            if(i+j==3 or i==j)
+            {
+             System.out.println("n[i][j]");
+            }
+            else
+            {
+                System.out.println(" ");
+            }
+        }
+        
+     }
+     System.out.println("\n");
+   }
+}
