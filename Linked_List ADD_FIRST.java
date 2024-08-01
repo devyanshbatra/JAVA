1ad48d6b4d987daafa952294c24dc033969c121e
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-08-02 00:31:07 +0530

    Add files via upload

---
+++ Linked_List ADD_FIRST.java
@@ -0,0 +1,27 @@
+class Linked_List{
+    class node{
+        String data;
+        Node.next;
+
+        Node(String data)
+        {
+        this.data=data;
+        this.next=null;
+        }
+
+    public void main addfirst(String data){
+        Node newNode = new Node(data)
+        if(head==null){
+            head=newNode
+            return0;
+        }
+    }   newNode.next=head;
+        head=newNode;
+
+    }
+    
+    public voide main (String args[])
+    l1=new Linked_List();
+    
+
+}
