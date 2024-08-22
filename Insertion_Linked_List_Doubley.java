7b2ae06060c7f54ed6a2ecc37c65ed73c88592d5
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-08-22 17:20:36 +0530

    Add files via upload

--- Insertion_Linked_List_Doubley.java
+++ Insertion_Linked_List_Doubley.java
@@ -1,30 +1,69 @@
-private static void print(Node head) {
-    while (head != null) {
-        // Print the data in the current node
-        System.out.print(head.data + " ");
-        // Move to the next node
-        head = head.next; // Move to the next node
-    }
-    System.out.println();
-}
 
-// Function to insert a new node with value 'k' at the end of the doubly linked list
+public class DLinkedList {
+    public static class Node {
+       
+        public int data;
+        
+        public Node next;
+       
+        public Node back;
+
+     
+        public Node(int data1, Node next1, Node back1) {
+            data = data1;
+            next = next1;
+            back = back1;
+        }
+
+        
+        public Node(int data1) {
+            data = data1;
+            next = null;
+            back = null;
+        }
+    }
+
+    private static Node convertArr2DLL(int[] arr) {
+       
+        Node head = new Node(arr[0]);
+        
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            Node temp = new Node(arr[i], null, prev);
+           
+            prev.next = temp;
+            
+            prev = temp;
+        }
+      
+        return head;
+    }
+
+    private static void print(Node head) {
+        while (head != null) {
+            
+            System.out.print(head.data + " ");
+            
+            head = head.next; 
+        }
+        System.out.println();
+    }
+}
 private static Node insertAtTail(Node head, int k) {
-    // Create a new node with data 'k'
+    
     Node newNode = new Node(k);
 
-    // If the doubly linked list is empty, set 'head' to the new node
     if (head == null) {
         return newNode;
     }
 
-    // Traverse to the end of the doubly linked list
     Node current = head;
     while (current.next != null) {
         current = current.next;
     }
 
-    // Connect the new node to the last node in the list
     current.next = newNode;
     newNode.back = current;
 
@@ -36,16 +75,16 @@ private static Node insertAtTail(Node head, int k) {
 
 public static void main(String[] args) {
     int[] arr = {12, 5, 6, 8, 4};
-    // Convert the array to a doubly linked list
+    
     Node head = convertArr2DLL(arr);
     
-    // Print the doubly linked list
+    
      System.out.println("Doubly Linked List Initially: ");
     print(head);
 
     System.out.println("Doubly Linked List After Inserting before the node with value 8:");
 
-    head = insertAtTail(head, 10); // Insert a node with value 10 at the end
+    head = insertAtTail(head, 10); 
     print(head);
 
 }
